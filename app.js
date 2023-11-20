a0d1e20d59042fa4f503651723b707e0b36aa37f
songowen <145769307+songowen@users.noreply.github.com>
2023-11-20 13:47:51 +0900

    chore: 프로젝트 세팅

---
+++ app.js
@@ -0,0 +1,13 @@
+import Express  from 'express';
+import { SERVER_PORT } from './constants/app.constant';
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
