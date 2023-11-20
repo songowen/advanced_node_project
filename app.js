b47fc2eb4fc3f66b8b7c380c4b59332777c7fefc
songowen <145769307+songowen@users.noreply.github.com>
2023-11-20 22:07:33 +0900

    api 업데이트

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-import Express  from 'express';
-import { SERVER_PORT } from './constants/app.constant';
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import { SERVER_PORT } from './constants/app.constant.js';
+import { apiRouter } from './routers/index.js';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use('/api', apiRouter);
+
+app.listen(SERVER_PORT, () => {
+  console.log(`App listening on port ${SERVER_PORT}`);
+});
